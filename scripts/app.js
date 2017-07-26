43f3daec36d7a9bd0fe88f4f3fff14a74806a4e0
Fabio Percivaldi Mr <fabio.percivaldi@ie.ibm.com>
2017-07-26 09:28:38 +0100

    prima release con cattura audio

---
+++ scripts/app.js
@@ -0,0 +1,74 @@
+// fork getUserMedia for multiple browser versions, for the future
+// when more browsers support MediaRecorder
+
+navigator.getUserMedia = ( navigator.getUserMedia ||
+                       navigator.webkitGetUserMedia ||
+                       navigator.mozGetUserMedia ||
+                       navigator.msGetUserMedia);
+
+// set up basic variables for app
+
+var record = document.querySelector('#start-recording');
+var stop = document.querySelector('#stop-recording');
+
+
+// visualiser setup - create web audio api context and canvas
+
+
+//main block for doing the audio recording
+
+if (navigator.getUserMedia) {
+   createUserMessage("getUserMedia supported.","");
+   navigator.getUserMedia (
+      // constraints - only audio needed for this app
+      {
+         audio: true
+      },
+
+      // Success callback
+      function(stream) {
+      	 var mediaRecorder = new MediaRecorder(stream);
+
+
+      	record.onclick = function() {
+			this.disabled = true;
+			stop.disabled = false;
+      	 	mediaRecorder.start();
+			createUserMessage("state: ", mediaRecorder.state);
+      	 	
+      	 	
+      	}
+
+      	stop.onclick = function() {
+			this.disabled = true;
+			record.disabled = false;
+      	 	mediaRecorder.stop();
+			createUserMessage("state: ", mediaRecorder.state);
+      	 	
+      	}
+		var chunks = [];
+
+		mediaRecorder.ondataavailable = function(e) {
+			chunks.push(e.data);
+		}
+		 
+		mediaRecorder.onstop = function(e) {
+			console.log("recorder stopped");
+			var blob = new Blob(chunks, { 'type' : 'audio/ogg; codecs=opus' });
+			chunks = [];
+			var audioURL = window.URL.createObjectURL(blob);
+			audio.src = audioURL;
+			createUserMessage("url blbo", 
+
+		}
+	  },
+      // Error callback
+		function(err) {
+			console.log('The following gUM error occured: ' + err);
+		}
+	  
+   );
+} else {
+  createUserMessage("getUserMedia not supported on your browser!","");
+}
+
